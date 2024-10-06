3cb7636566ec8feaf73bbed639db00e8567f743d
Ash530888 <aishah.s.qureshi@gmail.com>
2024-10-06 17:11:49 -0400

    settings.py corrected

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,12 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS=['localhost',
-                'https://aishahsqures-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-                ]
-                'aishahsqures-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',  # Add your domain here]
-CSRF_TRUSTED_ORIGINS=['https://aishahsqures-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-                    'aishahsqures-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',  # Add your domain here
-                    ]
+                'https://aishahsqures-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',]
+CSRF_TRUSTED_ORIGINS=['https://aishahsqures-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
